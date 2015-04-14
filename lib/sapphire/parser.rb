0c085d7945da7fe6696067166e5e53950a8ca5b0
Chris MacNaughton <chmacnaughton@gmail.com>
2015-04-14 09:46:49 -0400

    cleanup a bit

--- lib/sapphire/parser.rb
+++ lib/sapphire/parser.rb
@@ -27,14 +27,6 @@ module Sapphire
     end
 
     def parse_expression
-      parse_primary_expression
-    end
-
-    def parse_primary_expression
-      parse_modified_expression
-    end
-
-    def parse_modified_expression
       line_number = @token.line_number
 
       atomic = parse_op_assign
